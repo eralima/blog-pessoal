ef5e11e1ce05c13ce5d6b89c195c03ee9014cb3b
eralima <erikalmlima@gmail.com>
2021-05-27 13:29:05 -0300

    Métodos putPostagem e postPostagem na classe postagemController

--- src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -2,20 +2,29 @@ package org.generation.blogPessoal.controller;
 
 import java.util.List;
 
+
 import org.generation.blogPessoal.model.Postagem;
 import org.generation.blogPessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 //Crie um  controller
 
 @RestController
 @RequestMapping("/postagens")
+
+//
 @CrossOrigin("*")
 public class PostagemController {
 
@@ -25,12 +34,16 @@ public class PostagemController {
 	// Crie um método findAll
 
 	@GetMapping
+	
+	//ResponseEntity representa toda a resposta HTTP, ou seja, devolve como resposta o status code - informa o que aconteceu com a requisisição através de um valor que varia de 100 a 500 - headers 
+	//e body - onde geralmente enviamos dados que queremos gravar no banco de dados
 	public ResponseEntity<List<Postagem>> todasPostagens() {
 		return ResponseEntity.ok(repository.findAll());
 	}
 
 	// Crie um método findById
 	@GetMapping("/{id}")
+	//A anotação @PathVariable indica que o valor da variável virá da URL
 	public ResponseEntity<Postagem> postagemID(@PathVariable long id) {
 		return repository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.notFound().build());
@@ -41,4 +54,17 @@ public class PostagemController {
 	public ResponseEntity<List<Postagem>> postagemPeloTitulo(@PathVariable String titulo) {
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> alterar (@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+	
+	
+	
 }
--- src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -20,7 +20,7 @@ import javax.validation.constraints.Size;
 public class Postagem {
 	
 	@Id
-	//A anotação @GeneratedValue é utilizada para informar que a geração do valor do id será gerenciada pelo provedor de persistência (JPA)
+	//A anotation @GeneratedValue é utilizada para informar que a geração do valor do id será gerenciada pelo provedor de persistência (JPA Hibernate)
 	//Quando não anotamos o campo com essa opção, significa que a responsabilidade de gerar e gerenciar as chaves primárias do nosso código
 	//Caso optemos por modificar a estratégia de geração da chave primária, passamos essa informação no atributo strategy de @GeneratedValue
 	//GenerationType.IDENTITY informa ao provedor de persistência que os valores a serem atribuídos a chave primária serão gerados pela coluna de auto incremento do banco de dados. 
@@ -36,6 +36,9 @@ public class Postagem {
 	@Size(min = 10, max = 500)
 	private String texto;
 	
+	//A anotation @Temporal deve ser especificada para campos que são gerenciados pelo provedor de persistência (JPA Hibernate)
+	//ou propriedades do tipo java.util.Date ou java.util.Calendar.
+	//O TemporalType.TIMESTAMP indica o tipo 
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date data = new java.sql.Date(System.currentTimeMillis());
 	
